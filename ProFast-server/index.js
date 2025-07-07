b408a7b66c8a39757d8c0510858acb1c055fa082
programmarimran <programmarimran@gmail.com>
2025-07-08 01:14:33 +0600

    added a admin token verify c

--- ProFast-server/index.js
+++ ProFast-server/index.js
@@ -1,14 +1,56 @@
 const express = require("express");
 const cors = require("cors");
+const admin = require("firebase-admin");
 require("dotenv").config();
+
 const stripe = require("stripe")(process.env.PAYMENT_GATEWAY_KEY);
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 app.use(cors());
 app.use(express.json());
+//firebase SDK
+
+// const serviceAccount = require("./firebase-admin-key.json");
+// base64 থেকে JSON এ রূপান্তর
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_BASE64, "base64").toString(
+    "utf8"
+  )
+);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+//Firebase VerifyToken
+const tokenFbVerify = async (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "unauthorized access 1" });
+  }
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(token);
+    req.decoded = decodedToken;
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: "Forbidden Access" });
+  }
+};
+//admin verify
+const tokenAdmin = async (req, res, next) => {
+  const email = req?.decoded?.email;
+  console.log(email)
+  
+  const user = await usersCollection.findOne({ email: email });
+  if(!user){
+    return res.status(401).send({message:"unauthorize access 2"})
+  }
+  if(user && user.role !=="admin"){
+     return res.status(403).send({ message: "Forbidden - Not admin" });
+  }
+  next();
+};
 const uri = process.env.DB_URI;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -21,12 +63,17 @@ const client = new MongoClient(uri, {
 
 let parcelCollection = null;
 let paymentCollection = null;
+let usersCollection = null;
+let riderCollection = null;
 async function run() {
   try {
     // Connect to the "sample_mflix" database and access its "movies" collection
     const database = client.db("ProFastDB");
     parcelCollection = database.collection("parcels");
     paymentCollection = database.collection("payments");
+    usersCollection = database.collection("users");
+    riderCollection = database.collection("riders");
+
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
@@ -40,7 +87,33 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
+// *************************************************************************
+app.post("/users", async (req, res) => {
+  const user = req.body;
+  try {
+    // 1. Check if email exists
+    const existingUser = await usersCollection.findOne({ email: user.email });
+    if (existingUser) {
+      await usersCollection.updateOne(
+        { email: user.email },
+        { $set: { last_log_in: new Date().toISOString() } }
+      );
+      return res.status(409).json({ message: "User already exists" });
+    }
+    // 2. Insert new user
+    const result = await usersCollection.insertOne(user);
+    // 3. Return success
+    res.status(201).json({
+      message: "User created successfully",
+      insertedId: result.insertedId,
+    });
+  } catch (err) {
+    console.error("User insert error:", err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+// ***********************************************************************
+// parcels related api
 app.post("/parcels", async (req, res) => {
   const data = req.body;
   const result = await parcelCollection.insertOne(data);
@@ -51,7 +124,7 @@ app.get("/parcels", async (req, res) => {
   res.send(result);
 });
 // ✅ GET parcels by senderEmail
-app.get("/myparcels", async (req, res) => {
+app.get("/myparcels", tokenFbVerify, tokenAdmin, async (req, res) => {
   const email = req.query.email;
   if (!email) {
     return res.status(400).send({ error: "senderEmail query is required" });
@@ -74,11 +147,11 @@ app.delete("/parcels/:id", async (req, res) => {
   const result = await parcelCollection.deleteOne({ _id: new ObjectId(id) });
   res.send(result);
 });
-
+//*********************************************************************** */
 // created payment intregration system related api
 // ✅ GET /payments/user?email=imran@gmail.com
 
-app.get("/payments/user", async (req, res) => {
+app.get("/payments/user", tokenFbVerify, async (req, res) => {
   try {
     const userEmail = req.query.email;
 
@@ -121,7 +194,7 @@ app.post("/payments/record", async (req, res) => {
 
     // Prevent duplicate entries
     const exists = await paymentCollection.findOne({
-      paymentIntentId:paymentData.paymentIntentId,
+      paymentIntentId: paymentData.paymentIntentId,
     });
     if (exists) {
       return res.status(400).json({ message: "Payment already recorded." });
@@ -146,6 +219,82 @@ app.post("/payments/record", async (req, res) => {
       .status(500)
       .json({ message: "Error saving payment", error: err.message });
   }
+});
+//***************************Role protected************************************************* */
+// GET /api/users/role/:email
+app.get("/api/users/role/:email", async (req, res) => {
+  const user = await usersCollection.findOne({ email: req.params.email });
+  res.send({ role: user?.role || "user" });
+});
+
+//************************************************************************** */
+/* Admin related 🔍 Search users by (partial) email */
+app.get("/search", async (req, res) => {
+  const q = req.query.q?.trim();
+  if (!q) return res.status(400).json({ error: "query missing" });
+
+  const results = await usersCollection
+    .find({ email: { $regex: q, $options: "i" } })
+    // .project({ email: 1, createdAt: 1, role: 1 })   // শুধু দরকারি ফিল্ড
+    .limit(10)
+    .toArray();
+  res.json(results);
+});
+/* ⚙️  Set or remove admin role */
+app.patch("/:id/role", async (req, res) => {
+  const { id } = req.params;
+  const { role } = req.body; // "admin" বা "user"
+
+  if (!["admin", "user"].includes(role))
+    return res.status(400).json({ error: "invalid role" });
+
+  const result = await usersCollection.updateOne(
+    { _id: new ObjectId(id) },
+    { $set: { role } }
+  );
+  res.json(result);
+});
+//************************************************************************** */
+//rider collection
+app.post("/rider", async (req, res) => {
+  const rider = req.body;
+  rider.created_at = new Date().toISOString();
+  const result = await riderCollection.insertOne(rider);
+  res.send(result);
+});
+// GET /riders/pending
+app.get("/riders/pending",tokenFbVerify, tokenAdmin, async (req, res) => {
+  const pending = await riderCollection.find({ status: "pending" }).toArray();
+  res.send(pending);
+});
+//get /riders/active
+app.get("/riders/active",tokenFbVerify,tokenAdmin, async (req, res) => {
+  const activeRiders = await riderCollection
+    .find({ status: "active" })
+    .toArray();
+  res.send(activeRiders);
+});
+// PATCH /riders/approve/:id
+app.patch("/riders/approve/:id", async (req, res) => {
+  const id = req.params.id;
+  const { email } = req.body;
+  const result = await riderCollection.updateOne(
+    { _id: new ObjectId(id) },
+    { $set: { status: "active" } }
+  );
+  usersCollection.updateOne({ email: email }, { $set: { role: "rider" } });
+  res.send(result);
+});
+
+// Deactived /riders/:id
+app.patch("/riders/deactivate/:id", async (req, res) => {
+  const id = req.params.id;
+  const { email } = req.body;
+  const filter = { _id: new ObjectId(id) };
+  const updateDoc = { $set: { status: "deactivated" } };
+  const result = await riderCollection.updateOne(filter, updateDoc);
+  usersCollection.updateOne({ email: email }, { $set: { role: "user" } });
+  res.send(result);
 });
 
 app.listen(PORT, () => {
