e95c1248c3d44c080cfda4d105938b76209511c8
programmarimran <programmarimran@gmail.com>
2025-07-08 12:31:30 +0600

    adeded a  admin role added

--- ProFast-Client/src/hooks/useAxiosSecure.jsx
+++ ProFast-Client/src/hooks/useAxiosSecure.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import axios from "axios";
 import useAuth from "./useAuth";
+import { useNavigate } from "react-router";
 
 const axiosSecure = axios.create({
   baseURL: `http://localhost:3000`,
 });
 const useAxiosSecure = () => {
+  const navigate = useNavigate();
   const { user } = useAuth();
   axiosSecure.interceptors.request.use((config) => {
     config.headers.Authorization = `Bearer ${user.accessToken}`;
@@ -14,6 +16,22 @@ const useAxiosSecure = () => {
     (error) => {
       return Promise.reject(error);
     };
+  //********************* */
+  axiosSecure.interceptors.response.use((res) => {
+    return res;
+  },
+    (error) => {
+      console.log(error);
+      const status = error?.response?.status;
+      console.log(status);
+      if (status === 401 || status === 403) {
+        console.warn("🚫 Unauthorized or Forbidden, redirecting...");
+        navigate("/forbidden"); // ✅ Imperative redirect
+      }
+
+      return Promise.reject(error);
+    });
+
   return axiosSecure;
 };
 
